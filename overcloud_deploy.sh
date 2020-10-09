6e6249d4033e9882227a754e57b25d0d95a43854
zayednetad <nur.ahmed@outlook.com>
2020-10-09 16:55:51 -0500

    Added 2 lines in overcloud deploy script

--- overcloud_deploy.sh
+++ overcloud_deploy.sh
@@ -7,9 +7,11 @@ time openstack overcloud deploy \
   -r /home/stack/templates/roles_data.yaml \
   -n /home/stack/templates/network_data.yaml \
   -e /home/stack/containers-prepare-parameter.yaml \
-  -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.j2.yaml \
   -e /home/stack/templates/network-environment.yaml \
   -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.j2.yaml \
   -e /home/stack/templates/node-info.yaml \
   -e /home/stack/inject-trust-anchor-hiera.yaml \
   --ntp-server ntp-d.nist.gov \
