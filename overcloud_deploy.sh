75044131612c3ded34c41e9b92d0084b6ea31040
zayednetad <nur.ahmed@outlook.com>
2020-12-04 13:30:16 -0600

    Latest 120420

--- overcloud_deploy.sh
+++ overcloud_deploy.sh
@@ -8,11 +8,11 @@ time openstack overcloud deploy \
   -n /home/stack/templates/network_data.yaml \
   -e /home/stack/containers-prepare-parameter.yaml \
   -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
-  -e /home/stack/templates/network-environment.yaml \
   -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs.yaml \
   -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
   -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
   -e /home/stack/templates/node-info.yaml \
+  -e /home/stack/templates/network-environment.yaml \
   -e /home/stack/inject-trust-anchor-hiera.yaml \
   --ntp-server ntp-d.nist.gov \
   --timeout 90
